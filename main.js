14b88fb4906132384ba58e82709dafaea5dce92d
Gustavo <gustavobretas@gmail.com>
2020-04-11 14:23:22 -0400

    Herança de Classe

--- main.js
+++ main.js
@@ -10,11 +10,18 @@ class list {
 }
 
 class TodoList extends list {
-    
+    constructor(){
+        super();
+        this.usuario = 'Gustavo'
+    }
+    mostraUsuario() {
+        console.log(this.usuario);
+    }    
 }
-
 const MinhaLista = new TodoList();
 
 document.getElementById('novotodo').onclick = function(){
     MinhaLista.add('Novo Todo');
 }
+
+MinhaLista.mostraUsuario();
