384b544aa9c4f380d0f68c01d00c7efb18cf4741
Gustavo <gustavobretas@gmail.com>
2020-04-11 15:03:33 -0400

    Operadores REST/SPREAD

--- main.js
+++ main.js
@@ -25,3 +25,49 @@ document.getElementById('novotodo').onclick = function(){
 }
 
 MinhaLista.mostraUsuario();
+
+// Operadores rest/spread
+
+const usuario = {
+    nome: 'Gustavo',
+    idade: 35,
+    empresa: 'Shipinbox'
+}
+
+const { nome, ...resto } = usuario;
+
+console.log(nome);
+console.log(resto);
+
+// Array
+const arr = [1, 2, 3, 4];
+
+const [a, b, ...c] = arr
+
+console.log(a, b, c)
+
+function soma(...params) {
+    return params.reduce((total, next) => total + next);
+}
+
+console.log(soma(1,2,3,4,5,6));
+console.log(soma(1,2,3,4));
+
+// SPREAD Operator
+
+const arr1 = [1,2,3];
+const arr2 = [4,5,6];
+
+const arr3 = [...arr1, ...arr2];
+
+console.log(arr3)
+
+const usuario1 = {
+    nome: 'Gustavo',
+    idade: 35,
+    empresa: 'Shipinbox'
+}
+
+const usuario2 = {...usuario1, nome: 'Ricardo'}
+
+console.log(usuario2);
